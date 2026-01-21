ffde6822b903e31482c2ff382aa4178b31beaf0f
Ariel Martinez <arielmartinezlopez0@gmail.com>
2026-01-21 14:26:56 -0600

    a
    a

--- calculadora.js
+++ calculadora.js
@@ -1,14 +1,31 @@
 // comentario comentado
 alert("Adios mundo cruel")
-function suma() {
+function mostrar(e) {
     let numero1 = parseInt(document.getElementById("numero1").value);
     let numero2 = parseInt(document.getElementById("numero2").value);
     let result = document.getElementById("resultado");
     let resultado = 0;
+    let operacion = e.target.id;
  
-
-    resultado = (numero1 + numero2);
+    if (operacion == "suma"){
+     resultado = numero1 + numero2;   
+    }
     
+    else if (operacion == "resta"){
+        resultado = numero1 - numero2;
+    }
+
+    else if (operacion == "multiplica"){
+        resultado = numero1 * numero2;
+    }
+    else if (operacion == "divide"){
+        resultado = numero1 / numero2;
+    }   
+
+    else {operacion == "algoritmo"
+        alert("Funcionalidad en progreeso");
+
+    }
 
     result.innerHTML = resultado;
 
